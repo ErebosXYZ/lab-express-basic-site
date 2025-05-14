c35c282de969d6bf7f30d327973701269dd2fba5
ErebosXYZ <robertmaynou@gmail.com>
2025-05-14 20:23:54 +0200

    done

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ let viewsPath = path.join(__dirname, 'views');
 console.log(viewsPath);
 
 const app = express();
+app.use(express.static('public'));
+
 
 app.get("/", (req, res) => {
     res.sendFile(`${viewsPath}/home.html`);
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(`${viewsPath}/about.html`)
+});
+
+app.get("/works", (req, res) => {
+    res.sendFile(`${viewsPath}/works.html`)
 });
 
 app.listen(3000, ()=> {
